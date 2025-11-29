c21f4a9ba5dbeb0b511d3708f62b269676fdcb8e
yuchi <yuchiao0222@gmail.com>
2025-11-29 18:46:33 +0800

    Update members.js

--- members.js
+++ members.js
@@ -9,7 +9,7 @@ let students = [
     { name: "Yunfei Li", grade: "Master Student", major: "Software Engineering", group: "Member of Vision", img: "yunfeili.png" },
     { name: "Yeqing Sun", grade: "B.S. Student", major: "Mechanical Engineering", group: "Member of Manipulation and Hardware", img: "yeqingsun.jpg" },
     { name: "Yiru Liu", grade: "B.S. Student", major: "Automation", group: "Member of Manipulation", img: "yiruliu.jpeg" },
-    { name: "Han Ren", grade: "B.S. Student", major: "Mechanical Engineering", group: "Member of Simulation", img: "hanren.jpg" },
+    { name: "Han Ren", grade: "B.S. Student", major: "Mechanical Engineering", group: "Member of Navigation", img: "hanren.jpg" },
     { name: "Yuchiao Huang", grade: "B.S. Student", major: "Automation", group: "Member of Navigation", img: "yuchiaohuang.jpg" },  
     { name: "Zixu Wang", grade: "B.S. Student", major: "Automation", group: "Member of Manipulation and Vision", img: "zixuwang.jpg" } 
 ];
