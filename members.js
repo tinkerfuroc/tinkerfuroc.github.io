6c96b28f9f26f70e585b88e0be8d8d3f569431ba
yuchi <yuchiao0222@gmail.com>
2026-07-28 14:55:10 +0800

    Modify student group assignments and add new members
    Updated group assignments for Cindy Wang and Songchuan Lim. Added Yu'ang Yan and Ryowon An to the members list.

--- members.js
+++ members.js
@@ -1,18 +1,18 @@
 let students = [
     { name: "Kejin Wang(Team Leader)", grade: "B.S. Student", major: "Computer Science and Technology", group: "Head of Manipulation", img: "kejinwang.jpg" },
     { name: "Yeqing Sun(President)", grade: "B.S. Student", major: "Mechanical Engineering", group: "Member of Manipulation and Hardware", img: "yeqingsun.jpg" },
-    { name: "Cindy Wang", grade: "B.S. Student", major: "Software Engineering", group: "Head of Vision Group and Member of Decision", img: "cindywang.jpg" },
-    { name: "Songchuan Lim", grade: "B.S. Student", major: "Electronic Engineering and Technology", group: "Head of Hardware Group and Member of Decision", img: "songchuanlim.jpg" },
+    { name: "Cindy Wang", grade: "B.S. Student", major: "Software Engineering", group: "Head of Vision and Member of Decision", img: "cindywang.jpg" },
+    { name: "Songchuan Lim", grade: "B.S. Student", major: "Electronic Engineering and Technology", group: "Head of Hardware and Member of Decision", img: "songchuanlim.jpg" },
     { name: "Xinyao Qin", grade: "Ph.D. Student", major: "Automation", group: "Member of Manipulation", img: "xinyaoqin.jpg" },
     { name: "Mingkang Zhang", grade: "B.S. Student", major: "Weixian College", group: "Head of Navigation", img: "mingkangzhang.jpg" },
     { name: "Jiacheng Xie", grade: "B.S. Student", major: "Automation", group: "Head of Audio", img: "jiachengxie.jpg" },
     { name: "Rui Fang", grade: "B.S. Student", major: "Computer Science and Technology", group: "Head of Simulation", img: "ruifang.jpg" },
     { name: "Yunfei Li", grade: "Master Student", major: "Software Engineering", group: "Member of Vision", img: "yunfeili.png" },
     { name: "Yiru Liu", grade: "B.S. Student", major: "Automation", group: "Member of Manipulation", img: "yiruliu.jpeg" },
+    { name: "Yu'ang Yan", grade: "B.S. Student", major: "Mechanical Engineering", group: "Hardware", img: "yuangyan.jpg" },
     { name: "Han Ren", grade: "B.S. Student", major: "Mechanical Engineering", group: "Member of Navigation", img: "hanren.jpg" },
     { name: "Yuchiao Huang", grade: "B.S. Student", major: "Automation", group: "Member of Navigation", img: "yuchiaohuang.jpg" },  
     { name: "Zixu Wang", grade: "B.S. Student", major: "Automation", group: "Member of Manipulation and Vision", img: "zixuwang.jpg" },
-    { name: "Yu'ang Yan", grade: "B.S. Student", major: "Mechanical Engineering", group: "Hardware", img: "yuangyan.jpg" },
 ];
 
 let past_members = [
@@ -27,5 +27,6 @@ let past_members = [
     { name: "Sijia Xie", grade: "B.S. Student", major: "Mathematics and Applied Mathematics", group: "Vision", img: "sijiaxie.jpg" },
     { name: "Yimin Fang", grade: "B.S. Student", major: "Industrial Engineering", group: "Navigation", img: "yiminfang.jpg" },
     { name: "Ziwei Zhao", grade: "B.S. Student", major: "Automation", group: "Manipulation", img: "ziweizhao.jpg" },
-    { name: "Shiyu Zheng", grade: "B.S. Student", major: "Electrical Engineering & Its Automation", group: "Hardware", img: "shiyuzheng.jpg" }
+    { name: "Shiyu Zheng", grade: "B.S. Student", major: "Electrical Engineering & Its Automation", group: "Hardware", img: "shiyuzheng.jpg" },
+    { name: "Ryowon An", grade: "B.S. Student", major: "Computer Science and Technology", group: "Vision", img: " " },
 ];
